2e25bb3a1616dd1c0dba9143ee099b7124df7826
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2023-02-26 21:23:16 -0500

    Added code to connection.js

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+const Sequelize = require("sequelize");
+require('dotenv').config();
+
+// create connection to the db. Connect to JWASDB instance if env var provided.
+// (Heroku creates this variable when you provision a JAWSDB instance.)
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // use local .env file for connection arguments
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+
+module.exports = sequelize;
